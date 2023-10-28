7bd401c1b782b78531f9d2f8bce2fbb455a8fd13
quinnnaira <93191542+quinnnaira@users.noreply.github.com>
2023-10-28 07:17:59 +0100

    checkbox error resolved

--- src/TodoList.js
+++ src/TodoList.js
@@ -22,10 +22,11 @@ function TodoList() {
   const handleAddTask = () => {
     if (task) {
       const newTask = { id: nextId, text: task, completed: false, deleted: false };
+      const updatedTasks = [...tasks, newTask];
       setTask('');
-      setTasks([...tasks, newTask]);
+      setTasks(updatedTasks);
       setNextId(nextId + 1);
-      saveTasksToLocalStorage([...tasks, newTask]);
+      saveTasksToLocalStorage(updatedTasks); // Update local storage here
     }
   };
 
@@ -125,3 +126,56 @@ function TodoList() {
 }
 
 export default TodoList;
+
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+// function TodoList() {
+//   const [tasks, setTasks] = useState([]);
+//   const [task, setTask] = useState('');
+
+//   const handleAddTask = () => {
+//     if (task) {
+//       setTasks([...tasks, task]);
+//       setTask('');
+//     }
+//   };
+
+//   const handleDeleteTask = (index) => {
+//     const updatedTasks = tasks.filter((_, i) => i !== index);
+//     setTasks(updatedTasks);
+//   };
+
+//   return (
+//     <div className="container mt-4">
+//       <h2>My To-Do List</h2>
+//       <div className="input-group mb-3">
+//         <input
+//           type="text"
+//           className="form-control"
+//           placeholder="Add a task"
+//           value={task}
+//           onChange={(e) => setTask(e.target.value)}
+//         />
+//         <button className="btn btn-primary" onClick={handleAddTask}>
+//           Add
+//         </button>
+//       </div>
+
+//       <ul className="list-group">
+//         {tasks.map((task, index) => (
+//           <li className="list-group-item d-flex justify-content-between" key={index}>
+//             {task}
+//             <button
+//               className="btn btn-danger btn-sm"
+//               onClick={() => handleDeleteTask(index)}
+//             >
+//               Delete
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export default TodoList;
